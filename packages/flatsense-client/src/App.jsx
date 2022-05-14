96810eaea2f6dbf8d408fa5d812c7024b0aaedf6
jongkeun-kim <kjkandrea@gmail.com>
2022-05-14 17:41:35 +0900

    Add cute main page

--- packages/flatsense-client/src/App.jsx
+++ packages/flatsense-client/src/App.jsx
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import { getHelloWord } from '@flatsense/client-api';
-import Counter from '@flatsense/client-toolkit/src/components/Counter';
+import './assets/css/Main.css';
 import routes from './routes';
 import { useRoutes, BrowserRouter as Router } from 'react-router-dom';
 import Layout from './Layout';
@@ -10,35 +7,11 @@ const AppRoutes = () => useRoutes(routes);
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Router>
-          <AppRoutes />
-        </Router>
-      </Layout>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">{getHelloWord()}</h1>
-        <Counter />
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Layout>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </Layout>
   );
 }
 
--- packages/flatsense-client/src/pages/Main.jsx
+++ packages/flatsense-client/src/pages/Main.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import logo from '../assets/images/logo.svg';
+import '../assets/css/Main.css';
+import { getHelloWord } from '@flatsense/client-api';
+import Counter from '@flatsense/client-toolkit/src/components/Counter';
 
 export default function Main() {
-  return <div>is main page</div>;
+  return (
+    <div className="Main">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl font-bold underline">{getHelloWord()}</h1>
+        <Counter />
+        <p>is main page</p>
+        <p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
