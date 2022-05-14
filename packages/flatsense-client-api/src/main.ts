a867c7771136187b1c0191971e46e604dbeab3e6
jongkeun-kim <kjkandrea@gmail.com>
2022-05-14 15:59:24 +0900

    Add @flatsense/client-api structure

--- packages/flatsense-client-api/src/main.ts
+++ packages/flatsense-client-api/src/main.ts
@@ -1,8 +1,3 @@
-import './style.css'
+import {HelloText} from "../types";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+export const sayHello = (helloText: HelloText = 'Hello, flatsense! 🐶') => console.log(helloText)
---
+++ packages/flatsense-client-api/types/index.d.ts
@@ -0,0 +1,1 @@
+export type HelloText = string
