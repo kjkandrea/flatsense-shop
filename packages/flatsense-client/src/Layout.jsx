1e0f7c18e36c848f552f2a22edee26a50885fac1
jongkeun-kim <kjkandrea@gmail.com>
2022-05-21 14:23:36 +0900

    Add layout viewport

--- packages/flatsense-client/src/Layout.jsx
+++ packages/flatsense-client/src/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from './components/global/Footer';
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="global-wrapper">
       <Header />
       {children}
       <Footer />
