ebeae87d424285d41c77c55b409deba052560252
jongkeun-kim <kjkandrea@gmail.com>
2022-05-21 14:24:14 +0900

    Add curation page

--- packages/flatsense-client/src/routes/index.jsx
+++ packages/flatsense-client/src/routes/index.jsx
@@ -1,11 +1,16 @@
 import Main from '../pages/Main';
 import Product from '../pages/Product';
 import NotFound from '../pages/NotFound';
+import Curation from '../pages/Curation';
 
 export default [
   {
     path: '/',
     element: <Main />,
+  },
+  {
+    path: '/curation',
+    element: <Curation />,
   },
   {
     path: '/products/:id',
