c644c21261436e003037dee1f2a186a8da3706fe
jongkeun-kim <kjkandrea@gmail.com>
2022-06-03 17:43:04 +0900

    Add menu toggle icon

--- packages/flatsense-client/src/components/global/Header.jsx
+++ packages/flatsense-client/src/components/global/Header.jsx
@@ -9,16 +9,29 @@ export default function Header() {
     <header>
       <nav className="flex items-center justify-between flex-wrap p-4">
         <button onClick={() => setShowMenu((prev) => !prev)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
-          </svg>
+          {showMenu ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          )}
         </button>
         {showMenu && <div>나는야 메뉴</div>}
         <div>
