8ac487fc029e556f834cfe034e66e7bddea7eb04
jongkeun-kim <kjkandrea@gmail.com>
2022-06-03 17:16:13 +0900

    Remove trash

--- packages/flatsense-client/src/pages/Product.jsx
+++ packages/flatsense-client/src/pages/Product.jsx
@@ -14,11 +14,15 @@ export default function Product() {
         </div>
       </div>
       <div className="mt-3">
-        <div className="border-b border-gray-200 pb-6">
+        <div className="pb-6">
           <p className="text-sm leading-none text-gray-600 dark:text-gray-300 ">Shoes</p>
           <h1 className="lg:text-2xl text-xl font-semibold lg:leading-6 leading-7 text-gray-800 dark:text-white mt-2">
             Awesome Shoes
           </h1>
+        </div>
+        <div className="flex md:block border-b border-gray-200">
+          <div className="text-gray-900 text-lg font-semibold mr-2">$600.00</div>
+          <div className="text-gray-500 line-through text-lg font-semibold">$649.95</div>
         </div>
         <div className="py-4 border-b border-gray-200 flex items-center justify-between">
           <p className="text-base leading-4 text-gray-800 dark:text-gray-300">Size</p>
@@ -49,59 +53,6 @@ export default function Product() {
           <p className="md:w-96 text-base leading-normal text-gray-600 dark:text-gray-300 mt-4">
             Composition: 100% calf leather, inside: 100% lamb leather
           </p>
-        </div>
-        <div>
-          <div className="border-t border-b py-4 mt-7 border-gray-200">
-            <div data-menu className="flex justify-between items-center cursor-pointer">
-              <p className="text-base leading-4 text-gray-800 dark:text-gray-300">Shipping and returns</p>
-              <button
-                className="cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 rounded"
-                role="button"
-                aria-label="show or hide"
-              >
-                <img
-                  className="transform dark:hidden"
-                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/productDetail3-svg4.svg"
-                  alt="dropdown"
-                />
-                <img
-                  className="transform hidden dark:block"
-                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/productDetail3-svg4dark.svg"
-                  alt="dropdown"
-                />
-              </button>
-            </div>
-            <div className="hidden pt-4 text-base leading-normal pr-12 mt-4 text-gray-600 dark:text-gray-300" id="sect">
-              You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are
-              nonrefundable
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="border-b py-4 border-gray-200">
-            <div data-menu className="flex justify-between items-center cursor-pointer">
-              <p className="text-base leading-4 text-gray-800 dark:text-gray-300">Contact us</p>
-              <button
-                className="cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 rounded"
-                role="button"
-                aria-label="show or hide"
-              >
-                <img
-                  className="transform dark:hidden"
-                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/productDetail3-svg4.svg"
-                  alt="dropdown"
-                />
-                <img
-                  className="transform hidden dark:block"
-                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/productDetail3-svg4dark.svg"
-                  alt="dropdown"
-                />
-              </button>
-            </div>
-            <div className="hidden pt-4 text-base leading-normal pr-12 mt-4 text-gray-600 dark:text-gray-300" id="sect">
-              If you have any questions on how to return your item to us, contact us.
-            </div>
-          </div>
         </div>
       </div>
     </div>
